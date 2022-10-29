196f3a7c655ccce9ca60ef906db365f2c1f65b05
katopz <katopz@gmail.com>
2022-10-29 13:43:18 +0700

    feat: quiz tools

---
+++ en/assets/quiz.js
@@ -0,0 +1,129 @@
+const get_answers = (current) => {
+  let _current = current
+  let answers = []
+  while (_current.nextSibling && _current.nextSibling.nextSibling && _current.nextSibling.nextSibling.nodeName === 'PRE' && _current.nextSibling.nextSibling.firstChild.childNodes[1].className.indexOf('answer')) {
+    answers.push((_current = _current.nextSibling.nextSibling))
+  }
+
+  return answers
+}
+
+window.onload = function () {
+  const dmp = new diff_match_patch()
+
+  window.editors.forEach((editor, i) => {
+    // keep raw
+    editor.raw = editor.session.getValue()
+
+    // Clear marker on focus
+    function removeAllMarker(editor) {
+      const prevMarkers = editor.session.getMarkers()
+      if (prevMarkers) {
+        const prevMarkersArr = Object.keys(prevMarkers)
+        for (let item of prevMarkersArr) {
+          editor.session.removeMarker(prevMarkers[item].id)
+        }
+      }
+    }
+
+    editor.on('change', () => removeAllMarker(editor))
+
+    // Find answers after editor
+    let current = editor.container.parentElement.parentNode
+    editor.answers = get_answers(current)
+
+    // Create buttons from answers
+    let buttons = []
+    editor.answers.forEach((answer, i) => {
+      let button = document.createElement('button')
+      let button_text = `🦀 HINT` + (editor.answers.length === 1 ? `` : ` ${i + 1}`)
+      button.append(button_text)
+      button.className = 'hint'
+      button.alt = button_text
+      let uncompleted_text = editor.raw
+
+      button.onclick = () => {
+        // Revet
+        if (button.innerText.indexOf('🦀 HINT') === 0) {
+          button.innerText = `✨ UNDO`
+        } else {
+          button.innerText = button.alt
+          editor.setValue(uncompleted_text)
+          editor.selection.selectTo(0)
+          return
+        }
+
+        let answer_text = answer.firstChild.textContent
+
+        // Patch first comment to make hint easy to focus
+        if (uncompleted_text.indexOf('//') === 0) {
+          const comments = uncompleted_text.split('\n').filter((e) => e.indexOf('//') === 0)
+          answer_text = comments.join('\n') + '\n' + answer_text
+        }
+
+        // Patch first \ncomment to make hint easy to focus
+        if (uncompleted_text.indexOf('\n//') === 0) {
+          const comments = uncompleted_text.split('\n').filter((e) => e.indexOf('//') === 0)
+          answer_text = '\n' + comments.join('\n') + '\n' + answer_text
+        }
+
+        editor.setValue(answer_text)
+        editor.selection.selectTo(0)
+
+        // Highlight patched
+        const diffs = dmp.diff_main(uncompleted_text, answer_text)
+
+        if (diffs.length >= 0) {
+          let col = 0
+          let row = 0
+          diffs.forEach((diff) => {
+            let patch_type = diff['0']
+            let current_text = diff['1']
+            let hunks = current_text.split('\n')
+            let last_hunk = hunks[hunks.length - 1]
+            let last_hunk_padded = last_hunk.split('\t').join('')
+
+            const newline = Math.max(hunks.length - 1, 0)
+            let row0 = row
+            let column0 = col
+            let row1 = row + newline
+            let column1 = col + last_hunk_padded.length
+
+            if (patch_type === 1) {
+              let range = new ace.Range(row0, column0, row1, column1)
+              // skip comment
+              if (current_text.indexOf('//') !== 0) {
+                editor.session.addMarker(range, 'ace_step', 'line', false)
+              }
+
+              // Reset column
+              if (hunks.length > 1) {
+                col = 0
+              }
+
+              // Bump
+              col = col + last_hunk_padded.length
+              row = row + newline
+            } else if (patch_type === -1) {
+              // Do nothing
+            } else if (patch_type === 0) {
+              // Reset column
+              if (hunks.length > 1) {
+                col = 0
+              }
+
+              // Bump
+              col = col + last_hunk_padded.length
+              row = row + newline
+            }
+          })
+        }
+      }
+
+      buttons.push(button)
+      answer.remove()
+    })
+
+    buttons.forEach((button) => current.append(button))
+  })
+}
---
+++ tools/src/main.rs
@@ -0,0 +1,414 @@
+mod utils;
+use std::{fs, process::Command};
+
+use utils::{
+    get_folders, get_md_files, get_rs_files, is_path_exists, read_lines, write_file_md,
+    write_file_rs, ParseExpect, CODE_BEGIN_RE, CODE_END_RE, INSERTED_RS_RE, NUM_BULLET_RE,
+};
+
+fn generate_answer_rs(answer_file_name: &String) -> Result<(), anyhow::Error> {
+    let answer_file_names = answer_file_name.split("/").collect::<Vec<_>>();
+    let quiz_folder_name = if answer_file_names.len() == 4 {
+        format!(
+            "../en/src/{}",
+            answer_file_names[answer_file_names.len() - 2]
+        )
+    } else {
+        format!("../en/src")
+    };
+
+    let quiz_file_name = format!(
+        "{}/{}",
+        quiz_folder_name,
+        ((answer_file_names[answer_file_names.len() - 1])
+            .to_string()
+            .split_once(".")
+            .unwrap()
+            .0)
+            .to_string()
+    );
+
+    // Ensure md exist
+    let quiz_file_name_md = &format!("{}.md", quiz_file_name);
+
+    if !is_path_exists(quiz_file_name_md) {
+        panic!("not found {quiz_file_name_md}");
+    }
+
+    // --------------------- Generate rs ---------------------
+
+    let mut state = ParseExpect::Number;
+    let mut rust_content = "".to_owned();
+
+    let mut current_num_bullet = "".to_string();
+    let mut current_sub_index = 0;
+
+    // File hosts must exist in current path before this produces output
+    if let Ok(lines) = read_lines(answer_file_name) {
+        // Consumes the iterator, returns an (Optional) String
+        for line in lines {
+            match line {
+                Ok(text) => {
+                    // Process state
+                    match state {
+                        ParseExpect::Number => {
+                            // Begin with number and dot?
+                            if NUM_BULLET_RE.is_match(text.as_str()) {
+                                current_num_bullet = text.split_once(".").unwrap().0.to_string();
+                                state = ParseExpect::CodeBegin;
+                                continue;
+                            }
+
+                            // Begin other code block?
+                            if CODE_BEGIN_RE.is_match(text.as_str()) {
+                                // Bump next sub filename index
+                                current_sub_index = current_sub_index + 1;
+
+                                // Clear
+                                rust_content = "".to_owned();
+
+                                // Next
+                                state = ParseExpect::CodeEnd;
+                                continue;
+                            }
+                        }
+                        ParseExpect::CodeBegin => {
+                            // Begin code block?
+                            if CODE_BEGIN_RE.is_match(text.as_str()) {
+                                state = ParseExpect::CodeEnd;
+
+                                // New sub
+                                current_sub_index = 0;
+
+                                // Clear
+                                rust_content = "".to_owned();
+                                continue;
+                            }
+                        }
+                        ParseExpect::CodeEnd => {
+                            // Finish code block?
+                            if CODE_END_RE.is_match(text.as_str()) {
+                                // Write current block
+                                write_file_rs(
+                                    quiz_file_name.to_owned(),
+                                    &current_num_bullet,
+                                    current_sub_index,
+                                    &rust_content,
+                                )?;
+
+                                // Clear
+                                rust_content = "".to_owned();
+
+                                // Next
+                                state = ParseExpect::Number;
+                                continue;
+                            }
+
+                            rust_content.push_str(text.as_str());
+                            rust_content.push_str("\n".as_ref());
+                        }
+                    }
+                }
+                Err(_) => todo!(),
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn insert_answer_rs(answer_file_name: &String) -> Result<(), anyhow::Error> {
+    let answer_file_names = answer_file_name.split("/").collect::<Vec<_>>();
+    let quiz_folder_name = if answer_file_names.len() == 4 {
+        format!(
+            "../en/src/{}",
+            answer_file_names[answer_file_names.len() - 2]
+        )
+    } else {
+        format!("../en/src")
+    };
+
+    let file_name = answer_file_names[answer_file_names.len() - 1];
+    let quiz_file_name = format!(
+        "{}/{}",
+        quiz_folder_name,
+        (file_name.to_string().split_once(".").unwrap().0).to_string()
+    );
+
+    // Ensure md exist
+    let quiz_file_name_md = &format!("{}.md", quiz_file_name);
+
+    if !is_path_exists(quiz_file_name_md) {
+        panic!("not found {quiz_file_name_md}");
+    }
+
+    // ---------------------  Insert rs ---------------------
+
+    let rs_file_names = get_rs_files(&quiz_folder_name)?;
+    let file_name_md = answer_file_names[answer_file_names.len() - 1];
+    let file_name = file_name_md.split_once(".").unwrap().0.to_string();
+
+    let mut state = ParseExpect::Number;
+    let mut rust_content = "".to_owned();
+
+    let mut current_num_bullet = "".to_string();
+
+    // File hosts must exist in current path before this produces output
+    if let Ok(lines) = read_lines(&quiz_file_name_md) {
+        // Consumes the iterator, returns an (Optional) String
+        for line in lines {
+            match line {
+                Ok(text) => {
+                    // Keep all line, except inserted
+                    if !INSERTED_RS_RE.is_match(text.as_str()) {
+                        rust_content.push_str(text.as_str());
+                        rust_content.push_str("\n".as_ref());
+                    }
+
+                    // Process state
+                    match state {
+                        ParseExpect::Number => {
+                            // Begin with number and dot?
+                            if NUM_BULLET_RE.is_match(text.as_str()) {
+                                current_num_bullet = text.split_once(".").unwrap().0.to_string();
+                                state = ParseExpect::CodeBegin;
+                                continue;
+                            }
+                        }
+                        ParseExpect::CodeBegin => {
+                            // Begin code block?
+                            if CODE_BEGIN_RE.is_match(text.as_str()) {
+                                state = ParseExpect::CodeEnd;
+
+                                continue;
+                            }
+                        }
+                        ParseExpect::CodeEnd => {
+                            // Finish code block?
+                            if CODE_END_RE.is_match(text.as_str()) {
+                                // Insert answer
+                                let base_file_name = format!("{file_name}_{current_num_bullet}_");
+                                let mut is_push = false;
+
+                                // Match answer(s)
+                                rs_file_names
+                                    .iter()
+                                    .filter(|e| e.starts_with(&base_file_name))
+                                    .for_each(|e| {
+                                        // {{#playground statements_1_0.rs answer}}
+                                        rust_content.push_str(
+                                            format!("\n{{{{#playground {e} answer}}}}").as_str(),
+                                        );
+
+                                        is_push = true
+                                    });
+
+                                if is_push {
+                                    rust_content.push_str(format!("\n").as_str())
+                                }
+
+                                // Next
+                                state = ParseExpect::Number;
+                                continue;
+                            }
+                        }
+                    }
+                }
+                Err(_) => todo!(),
+            }
+        }
+    }
+
+    Ok(write_file_md(&quiz_file_name_md, &rust_content)?)
+}
+
+fn generate_solution(path_string: &str) -> Result<(), anyhow::Error> {
+    // All files
+    let base_path = format!("{path_string}");
+    let md_file_names = get_md_files(&path_string.to_owned()).unwrap();
+    println!("process:{:?}", base_path);
+
+    // TODO: handle result
+    let _results = md_file_names
+        .iter()
+        .map(|file_name| {
+            let file_path = format!("{base_path}/{file_name}");
+            generate_answer_rs(&file_path).unwrap();
+            insert_answer_rs(&file_path).unwrap();
+        })
+        .collect::<Vec<_>>();
+
+    // All folders
+    let folders = get_folders(&path_string.to_string())?;
+
+    folders.iter().for_each(|folder| {
+        let base_path = format!("{path_string}/{folder}");
+        let md_file_names = get_md_files(&base_path.to_owned()).unwrap();
+        println!("process:{:?}", base_path);
+
+        // TODO: handle result
+        let _results = md_file_names
+            .iter()
+            .map(|file_name| {
+                let file_path = format!("{base_path}/{file_name}");
+                generate_answer_rs(&file_path).unwrap();
+                insert_answer_rs(&file_path).unwrap();
+            })
+            .collect::<Vec<_>>();
+    });
+
+    Ok(())
+}
+
+#[allow(dead_code)]
+fn format_md_rs(file_path: &String) -> Result<(), anyhow::Error> {
+    let mut state = ParseExpect::Number;
+    let mut md_content = "".to_owned();
+    let mut rust_content = "".to_owned();
+
+    let mut current_num_bullet = "".to_string();
+
+    // File hosts must exist in current path before this produces output
+    if let Ok(lines) = read_lines(&file_path) {
+        // Consumes the iterator, returns an (Optional) String
+        for line in lines {
+            match line {
+                Ok(text) => {
+                    // Keep all content except code block
+                    if state == ParseExpect::Number {
+                        md_content.push_str(text.as_str());
+                        md_content.push_str("\n".as_ref());
+                    }
+
+                    // Process state
+                    match state {
+                        ParseExpect::Number => {
+                            // Begin with number and dot?
+                            if NUM_BULLET_RE.is_match(text.as_str()) {
+                                current_num_bullet = text.split_once(".").unwrap().0.to_string();
+                                state = ParseExpect::CodeBegin;
+                                continue;
+                            }
+                        }
+                        ParseExpect::CodeBegin => {
+                            // Keep
+                            md_content.push_str(text.as_str());
+                            md_content.push_str("\n".as_ref());
+
+                            // Begin code block?
+                            if CODE_BEGIN_RE.is_match(text.as_str()) {
+                                state = ParseExpect::CodeEnd;
+
+                                continue;
+                            }
+                        }
+                        ParseExpect::CodeEnd => {
+                            // Finish code block?
+                            if CODE_END_RE.is_match(text.as_str()) {
+                                // Format
+                                let temp_file_path = "temp.md".to_owned();
+                                write_file_md(&temp_file_path, &rust_content)?;
+                                let mut format_cmd = Command::new("rustfmt");
+                                format_cmd.arg("-v").arg(&temp_file_path);
+                                let format_output =
+                                    format_cmd.output().expect("failed to execute format");
+                                if format_output.stderr.len() > 0 {
+                                    println!(
+                                        "{:#?}",
+                                        String::from_utf8(format_output.stderr).unwrap()
+                                    )
+                                }
+                                let formatted_rust_content = fs::read_to_string(&temp_file_path)
+                                    .expect("Should have been able to read the file");
+                                fs::remove_file(temp_file_path)?;
+
+                                // Merge code
+                                md_content.push_str(formatted_rust_content.as_str());
+                                md_content.push_str(text.as_str());
+                                md_content.push_str("\n".as_ref());
+
+                                println!("formatted: {current_num_bullet:?}");
+
+                                // Next
+                                state = ParseExpect::Number;
+                                rust_content = "".to_owned();
+                                continue;
+                            }
+
+                            // Keep code
+                            rust_content.push_str(text.as_str());
+                            rust_content.push_str("\n".as_ref());
+                        }
+                    }
+                }
+                Err(_) => todo!(),
+            }
+        }
+    } else {
+        println!("not ok");
+    }
+
+    if md_content.len() > 0 {
+        Ok(write_file_md(&format!("{file_path}"), &md_content)?)
+    } else {
+        Ok(())
+    }
+}
+
+#[allow(dead_code)]
+fn format_md_rs_in_folder(path_string: &str) -> Result<(), anyhow::Error> {
+    // All files
+    let base_path = format!("{path_string}");
+    let md_file_names = get_md_files(&path_string.to_owned()).unwrap();
+
+    // TODO: handle result
+    let _results = md_file_names
+        .iter()
+        .map(|file_name| {
+            let file_path = format!("{base_path}/{file_name}");
+            println!("format:{:?}", file_path);
+            format_md_rs(&file_path).unwrap();
+        })
+        .collect::<Vec<_>>();
+
+    // All folders
+    let folders = get_folders(&path_string.to_string())?;
+
+    // TODO : DRY
+    folders.iter().for_each(|folder| {
+        let base_path = format!("{path_string}/{folder}");
+        let md_file_names = get_md_files(&base_path.to_owned()).unwrap();
+
+        // TODO: handle result
+        let _results = md_file_names
+            .iter()
+            .map(|file_name| {
+                let file_path = format!("{base_path}/{file_name}");
+                println!("format:{:?}", file_path);
+                format_md_rs(&file_path).unwrap();
+            })
+            .collect::<Vec<_>>();
+    });
+
+    Ok(())
+}
+
+fn main() -> Result<(), anyhow::Error> {
+    // format_md_rs(&"./solutions/basic-types/numbers.md".to_string())
+    // format_md_rs(&"./solutions/collections/Vector.md".to_string())
+    // format_md_rs(&"./solutions/ownership/ownership.md".to_string())
+
+    // format_md_rs(&"./en/src/type-conversions/others.md".to_string())
+    // format_md_rs(&"./en/src/pattern-match/match-iflet.md".to_string())
+    // format_md_rs(&"./en/src/pattern-match/patterns.md".to_string())
+
+    // format_md_rs_in_folder("./solutions")
+    // format_md_rs_in_folder("./en/src")
+
+    // format_md_rs_in_folder("./en/src/basic-types")
+
+    // format_md_rs(&"./en/src/method.md".to_string())
+    // format_md_rs(&"./en/src/flow-control.md".to_string())
+
+    generate_solution("../solutions")
+}
